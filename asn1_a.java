5b0702509cff76fc4351ece1af3a954f3938ebfe
Christopher Tam <ctam86@uwo.ca>
2019-02-03 16:41:52 -0500

    push asn1_a

--- asn1_a.java
+++ asn1_a.java
@@ -1,10 +1,9 @@
 public class asn1_a {
 
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
-
    public static void main(String []args) {
+     /**
+     * Case 1: j =  20000
+     **/
      int j = 20000;
      //Create a reversely sorted integer array of size 20000
       int[] myList = new int[20001];
@@ -18,12 +17,37 @@ public class asn1_a {
         System.out.println(myList[i]);
       }
 
+      // Perform insertion sort on myList
       int[] arr2 = insertionSort(myList);
 
       // Print out first 20 integers of array after insertion sort
       for (int i = 0; i < 20; i++) {
         System.out.println(myList[i]);
       }
+
+      /**
+      * Case 2: j =  200000
+      **/
+      int j = 200000;
+      //Create a reversely sorted integer array of size 20000
+       int[] myList = new int[200001];
+       for (int i = 0; i < 200000; i++) {
+         myList[i] = j;
+         j--;
+       }
+
+       // Print out first 20 integers of array before insertion sort
+       for (int i = 0; i < 20; i++) {
+         System.out.println(myList[i]);
+       }
+
+       // Perform insertion sort on myList
+       int[] arr2 = insertionSort(myList);
+
+       // Print out first 20 integers of array after insertion sort
+       for (int i = 0; i < 20; i++) {
+         System.out.println(myList[i]);
+       }
 
     }
 
